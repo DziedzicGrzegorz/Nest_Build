6b5b1fd2626fa1c682055d09b4de7068b0248cde
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2023-12-21 14:51:11 +0100

    testing git hooks

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'testting githubAction';
+    return 'testting git hooks';
   }
 }
